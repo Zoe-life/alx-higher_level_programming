2130c67ccead73845c3d9c29e2a960e784977050
zoe-life <merlynzaw@gmail.com>
2024-05-19 08:14:19 -0700

    Fix: handling of negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number_str = str(number)
-if number_str[-1] > '5':
-    print(f"Last digit of {number} is {number_str[-1]} and is greater than 5")
-elif number_str[-1] == '0':
-    print(f"Last digit of {number} is {number_str[-1]} and is 0")
-else:
-    print(f"Last digit of {number} is {number_str[-1]} and is less than 6 and not 0")
+
+
+def last_digit(number):
+    """ Handling negatives """
+    if number >= 0:
+        return number % 10
+    else:
+        return -(number % -10)
+
+
+last = last_digit(number)
+number_str = str(last)
+if number_str > '5':
+    print(f"Last digit of {number} is {number_str}\
+            and is greater than 5")
+elif number_str == '0':
+    print(f"Last digit of {number} is {number_str}\
+            and is 0")
+elif number:
+    print(f"Last digit of {number} is {number_str}\
+            and is less than 6 and not 0")
