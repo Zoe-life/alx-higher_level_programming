dcad54e52ccc94037fd51a3fd8ffca5f1f718228
zoe-life <merlynzaw@gmail.com>
2024-05-19 10:25:24 -0700

    Fix: sign handling of negatives

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,15 +5,22 @@ number = random.randint(-10000, 10000)
 
 def last_digit(number):
     """ Handling negatives """
-    return number % 10 if number >= 0 else - (number % -10)
+    last_digit_1 = number % 10 if number >= 0 else -(number % -10)
+    return str(last_digit_1)
 
 
-last = last_digit(number)
-number_str = str(last)
+# Extract the sign from the original number
+sign = "-" if number < 0 else ""  # Empty string for positive numbers
+
+
+last_digit_str = last_digit(number)
 number_str1 = str(number)
-if number_str1 > '0' and number_str > '5':
-    print(f"""Last digit of {number} is {number_str} and is greater than 5""")
-elif number_str1 >= '0' and number_str == '0':
-    print(f"""Last digit of {number} is {number_str} and is 0""")
+if number_str1 > '0' and last_digit_str > '5':
+    print(f"Last digit of {number} is {sign}{last_digit_str}"
+          " and is greater than 5")
+elif number_str1 >= '0' and last_digit_str == '0':
+    print(f"Last digit of {number} is {sign}{last_digit_str}"
+          " and is 0")
 else:
-    print(f"""Last digit of {number} is {number_str} and is less than 6 and not 0""")
+    print(f"Last digit of {number} is {sign}{last_digit_str}"
+          " and is less than 6 and not 0")
