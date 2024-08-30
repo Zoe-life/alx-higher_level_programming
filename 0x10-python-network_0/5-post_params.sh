9c28ef6355c32551b8ace5a2cd344a83da1b6e4c
Merlyn Zawadi <127236012+Zoe-life@users.noreply.github.com>
2024-08-30 04:45:12 +0300

    Update: 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # This script sends a POST request with form data and displays the response body using curl.
-
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-body=$(curl -s -X POST -d "email=$email&subject=$subject" "$1")
-echo "POST params:"
-echo "    email: $email"
-echo "    subject: $subject"
-echo ""
-echo "$body"
+curl -s -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1" && (echo "POST params:"; echo "    email: test@gmail.com"; echo "    subject: I will always be here for PLD"; echo ""; echo "$body")
