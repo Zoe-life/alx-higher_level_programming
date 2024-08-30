0aca5efafcdedd73014baa1b814b568d9444e58d
Merlyn Zawadi <127236012+Zoe-life@users.noreply.github.com>
2024-08-30 04:24:48 +0300

    Update: 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 # This script sends a request to a URL and checks for a specific message in the response using curl.
-
-url="http://0.0.0.0:5000/catch_me"
-
-# Send request and capture response body
-body=$(curl -s "$url")
-
-# Check if response body contains the target message
-if grep -q "You got me!" <<< "$body"; then
-  exit 0  # Exit with success code (0) if message is found
-fi
-
-exit 1  # Exit with failure code (1) if message is not found
+url="http://0.0.0.0:5000/catch_me"; [[ $(curl -s "$url") =~ "You got me!" ]] && exit 0 || exit 1
