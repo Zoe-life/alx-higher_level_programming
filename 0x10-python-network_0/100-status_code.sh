b8aa146193038f9f20ddf84db575365578641d0b
Merlyn Zawadi <127236012+Zoe-life@users.noreply.github.com>
2024-08-30 03:42:12 +0300

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # This script retrieves the status code of a URL request using curl.
-
-status_code=$(curl -s -w "%{http_code}" "$1")
-
-if [[ $status_code ]]; then
-  echo "$status_code"
-fi
+response=$(curl -sI "$1"); echo "${response: -3}"
