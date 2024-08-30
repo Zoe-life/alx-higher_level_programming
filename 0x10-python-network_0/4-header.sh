28bc8d455c74a53447f6d29aab7a53b61abea30c
Merlyn Zawadi <127236012+Zoe-life@users.noreply.github.com>
2024-08-30 04:33:36 +0300

    Update: 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script sends a GET request with a custom header and displays the response body using curl.
-
-body=$(curl -s -H "X-School-User-Id: 98" "$1")
-echo "$body"
+body=$(curl -s -H "X-School-User-Id: 98" "$1"); echo "$body"
