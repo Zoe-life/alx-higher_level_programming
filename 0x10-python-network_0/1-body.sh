3c299a550f1d5f94ecfed07fa8e9f5b15dd9e3e4
Merlyn Zawadi <127236012+Zoe-life@users.noreply.github.com>
2024-08-30 03:27:29 +0300

    Update: 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Get response body for successful (200) GET requests
-response=$(curl -s "$1"); [[ "${response: -3}" == 200 ]] && echo "${response:0:-3}"
+response=$(curl -sL "$1"); [[ "${response: -3}" == 200 ]] && echo "${response:0:-3}"
