1102356e931ecada920172e37ec0a534ce3d135a
Merlyn Zawadi <127236012+Zoe-life@users.noreply.github.com>
2024-08-30 04:25:44 +0300

    Update: 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script sends a DELETE request to a URL and displays the response body using curl.
-
-body=$(curl -s -X DELETE "$1")
-echo "$body"
+body=$(curl -s -X DELETE "$1"); echo "$body"
